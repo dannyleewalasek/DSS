21dab026851d6fe469bc802e5e700894068ff9cd
DannyWalasek <d.l.walasek@liverpool.ac.uk>
2020-01-09 19:52:36 +0000

    probabilitys

--- modeltraining.m
+++ modeltraining.m
@@ -13,17 +13,32 @@ plot(C(:,1),C(:,2),'b*','MarkerSize',5);
 %Determine class of each point.
 for c = 1:height(trainingData)
     minDistance = 99999999999999999999999999;
-    disp("newcheck");
     for d = 1:size(C)
-        disp("loop eneterd");
         disp(sqrt((C(d,1) - trainingData(c,:).BurglaryCount).^2 + (C(d,2) - trainingData(c,:).Mean_Index).^2));
         if sqrt((C(d,1) - trainingData(c,:).BurglaryCount).^2 + (C(d,2) - trainingData(c,:).Mean_Index).^2) < minDistance
             trainingData(c,:).class = d;
-            disp("training " + c + " assigned to " + d);
             minDistance = sqrt((C(d,1) - trainingData(c,:).BurglaryCount).^2 + (C(d,2) - trainingData(c,:).Mean_Index).^2);
         end
     end
 end
+
+burg = [0,0,0,0,0;
+        0,0,1,0,0;
+        0,0,2,0,0;
+        0,0,0,0,0];
+for e = 1:4
+    burg(c,1) = 1/4 * e;
+end
+
+%2 tables, one with burglary numbers say 0-5, 5-10, etc 4 of them, 
+%with likelyhood of being in each class
+%second table is with index ranges 4 of them and likelyhood
+% of being in each class
+
+%Num of burglarys probability e.g:
+%Range | P(class1) | P(class2) | P(class3) | P(class4)
+%-----------------------------------------------------
+%0 - 5 | 0.2       |0.1        | 0.6       | 0.1
 
 %FREQUENCY TABLE CREATION TO BE USED WITH NAIVE BAYES 
 %Save training data with class identification
