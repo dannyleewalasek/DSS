231075fe7d7971a8963ad6397a070eaf248495c5
DannyWalasek <d.l.walasek@liverpool.ac.uk>
2020-01-11 15:29:47 +0000

    bug
    works fine for first test not second but same action

--- AccuracyTest.m
+++ AccuracyTest.m
@@ -6,30 +6,58 @@
 
 testdata = readtable('testdata.xlsx');
 burglaryProbabilities = readtable('burglaryProbabilities');
+indexProbabilities = readtable('indexProbabilities');
 
 % Accuracy test against burglary numbers
-incorrect = 0;
-correct = 0;
+bincorrect = 0;
+bcorrect = 0;
+iincorrect = 0;
+icorrect = 0;
 class = 1;
 currentProbability = 0;
 for a = 1:height(testdata)
-    for b = 1:4
-        if  b == 4
-            class = 4;
-        elseif b == 1
-            currentProbability = burglaryProbabilities(b,:).Class1;
-        elseif testdata(a,:).index <= burglaryProbabilities(b,:).Range
-            for c = 2:5
-                if burglaryProbabilities(b,c) > currentProbability
-                    currentProbability = burglaryProbabilities(b,c);
-                    class = c;
+    if ~isnan(testdata(a,:).Burglarys)
+        for b = 1:4
+            if  b == 4
+                class = 4;
+            elseif b == 1
+                currentProbability = burglaryProbabilities(b,:).Class1;
+            elseif testdata(a,:).index <= burglaryProbabilities(b,:).Range
+                for c = 2:5
+                    if burglaryProbabilities(b,c) > currentProbability
+                        currentProbability = burglaryProbabilities(b,c);
+                        class = c;
+                    end
                 end
             end
         end
-    end
-    if class == testdata(a,:).ActualClass
-        correct = correct +1;
+             if class == testdata(a,:).ActualClass
+                bcorrect = bcorrect +1;
+             else
+                bincorrect = bincorrect +1;
+            end
     else
-        incorrect = correct +1;
+            for b = 1:4
+                if  b == 4
+                  class = 4;
+                elseif b == 1
+                    currentProbability = indexProbabilities(b,:).Class1;
+                elseif testdata(a,:).index <= indexProbabilities(b,:).Range
+                    for c = 2:5
+                        if indexProbabilities(b,c) > currentProbability
+                            currentProbability = indexProbabilities(b,c);
+                            class = c;
+                        end
+                    end
+                end
+            end
+            if class == testdata(a,:).ActualClass
+                icorrect = icorrect +1;
+            else
+                iincorrect = iincorrect +1;
+            end
     end
 end
+
+bar(categorical({'BCorrect','BIncorrect','ICorrect','IIncorrect'}),[bcorrect, bincorrect,icorrect, iincorrect]);
+clear;
