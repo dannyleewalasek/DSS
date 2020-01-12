20731c49f561957b086bdeab12e7d0a069bec3c7
DannyWalasek <d.l.walasek@liverpool.ac.uk>
2020-01-12 15:56:30 +0000

    needs more randomness

--- createtrainingdata.m
+++ createtrainingdata.m
@@ -6,17 +6,19 @@ crimeStats = table("LS8_1PR", "Burglary");
 crimeStats.Properties.VariableNames = {'PostCode','CrimeType'};
 housePrices = table("LS8_1PR", "250000", "5000");
 housePrices.Properties.VariableNames = {'PostCode','HousePrice','ContentsValue'};
-
 % These values need some randomness and some outliers
-for a = 1:100
-    for b = 1:a
+a = 1;
+while a < 1000
+    c = a + randi(5);
+    for b = a:c
         crimeStats{a+b,1} = strcat("LS",num2str(a),"1QE");
         housePrices{a+b,1} = "LS"+num2str(a)+"1QE";
         % make some of these not burglary
         crimeStats(a+b,:).CrimeType = "Burglary";
-        housePrices(a+b,:).HousePrice = a * 10000;
-        housePrices(a+b,:).ContentsValue = a * 10000;
+        housePrices(a+b,:).HousePrice = a * 10000 * (rand);
+        housePrices(a+b,:).ContentsValue = 1000 + (a * 10 * (rand));
     end
+    a = c-1;
 end
 writetable(housePrices);
 writetable(crimeStats);
