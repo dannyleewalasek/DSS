cd7418a444e7259a1739bcbac0331be765be30b8
DannyWalasek <d.l.walasek@liverpool.ac.uk>
2020-01-12 20:51:48 +0000

    stuff

--- CreateData.m
+++ CreateData.m
@@ -9,19 +9,17 @@ housePrices.Properties.VariableNames = {'PostCode','HousePrice','ContentsValue'}
 % These values need some randomness and some outliers
 a = 2;
 while a < 900
-    c = a + randi(round(100-(a/10))) + 1;
+    c = a + randi(round(50-(a/20))) + 1;
     for b = a:c
         testData{b,1} = strcat("LS",num2str(a),"1QE");
         crimeStats{b,1} = strcat("LS",num2str(a),"1QE");
         housePrices{b,1} = "LS"+num2str(a)+"1QE";
         % make some of these not burglary
         crimeStats(b,:).CrimeType = "Burglary";
-        housePrices(b,:).HousePrice = a * 1000;
-        housePrices(b,:).ContentsValue = a + 1000 ;
-        testData{b,2} = (a * 10) + ( a + 1000);
+        housePrices(b,:).HousePrice = b * 1000;
+        housePrices(b,:).ContentsValue = b + 1000 ;
+        testData{b,2} = ((b * 1000)/100) + ( b + 1000);
         disp("writing to: " + b);
-    end
-    for b = a:c
         testData{b,3} = c - a;
         testData{b,4} = 0;
     end
