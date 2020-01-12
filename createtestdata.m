00df0ad1f3b645280f3b11a62534e9e1ca5b222b
DannyWalasek <d.l.walasek@liverpool.ac.uk>
2020-01-12 17:42:07 +0000

    gg

--- createtestdata.m
+++ createtestdata.m
@@ -6,8 +6,8 @@ testdata = table("LS8_1PR", "1000", 10, 0);
 testdata.Properties.VariableNames = {'PostCode','index','Burglarys','ActualClass'};
 for a = 1:100
     testdata{a,1} = strcat("LS8",num2str(a),"PR");
-    testdata{a,2} = a * 100;
-    testdata{a,3} = a;
+    testdata{a,2} = 11000 - (a * 90);
+    testdata{a,3} = a * 100;
     testdata{a,4} = 0;
 end
 
