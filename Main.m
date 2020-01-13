d15ebc9ea09c0a0d7acbaae73007965cfbd80800
DannyWalasek <d.l.walasek@liverpool.ac.uk>
2020-01-13 21:22:39 +0000

    minor

--- Main.m
+++ Main.m
@@ -1,7 +1,8 @@
 %{
-This script is used to launch all script in turn and run a whole cycle of
+This script is used to launch all scripts in turn and run a whole cycle of
 the system
 %}
+
 % Data creation
 CreateData;
 % Prepare the data
